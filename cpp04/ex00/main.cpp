53672e216915a6eea2f77d17621f3fa6ae13873f
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2025-12-07 20:38:07 +0000

    finish cpp04/ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,20 +1,44 @@
 
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongCat.hpp"
+#include "WrongAnimal.hpp"
 
-int main()
+
+void testSubject()
 {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
+    i->makeSound();
     j->makeSound();
     meta->makeSound();
 
     delete meta;
     delete j;
     delete i;
+}
+
+void testSubjectWrongCat()
+{
+    const WrongAnimal* wrongAnimal = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+    std::cout << wrongAnimal->getType() << " " << std::endl;
+    std::cout << wrongCat->getType() << " " << std::endl;
+    wrongAnimal->makeSound();
+    wrongCat->makeSound();
+
+    delete wrongAnimal;
+    delete wrongCat;
+}
+
+int main()
+{
+    testSubject();
+    std::cout << "\n=========================================\n";
+    std::cout << "=========================================\n\n";
+    testSubjectWrongCat();
     return 0;
 }
