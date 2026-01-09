c09d7322daf970349370f531da4cf32d9fa8521a
Tiago <TiagoDev@TiagoDev.com>
2026-01-09 12:04:42 +0000

    intern.cpp

---
+++ cpp05/ex03/src/Intern.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include "../inc/Intern.hpp"
+
+Intern::Intern() {};
+
+Intern::Intern(const Intern& other)
+{
+
+}
+
+Intern& Intern::operator=(const Intern& other)
+{
+
+}
+
+Intern::~Intern(){}
+
+AForm* Intern::makeForm(const std::string& name_form, const std::string& target) const
+{
+
+}
