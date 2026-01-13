7674bc4cdee1575be6eba0eceb21859c2dc88407
Tiago <TiagoDev@TiagoDev.com>
2026-01-13 11:46:43 +0000

    cpp06/ex00 init main

---
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -0,0 +1,16 @@
+
+
+#include "../inc/ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other) { *this = other; }
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) { return (*this); }
+
+ScalarConverter::~ScalarConverter() {}
+
+void ScalarConverter::convert(const std::string str)
+{
+    
+}
