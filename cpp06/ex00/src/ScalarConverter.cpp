668ccab48c976b8a4c61c56f64844b3115457a78
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2026-08-26 23:22:30 +0100

    refactor cpp06/ex00: update getType function to improve type detection logic and add print functions

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -14,7 +14,7 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
 
 ScalarConverter::~ScalarConverter() {}
 
-int getType(const std::string &str, size_t len)
+static int getType(const std::string &str, size_t len)
 {
   if (str == "+inf" || str == "-inf" || str == "+inff" || str == "-inff" || str == "nan" || str == "nanf")
     return SPECIAL;
@@ -27,84 +27,116 @@ int getType(const std::string &str, size_t len)
   if (dot == std::string::npos || len == 1)
   {
     // aqui vai ser int, char
-    if ((len == 1 && !std::isdigit(str[0])) || (len == 3 && str[0] == '\'' && str[2] == '\''))
-    {
-      std::cout << "CHAR\n";
+    if ((len == 1 && !std::isdigit(static_cast<unsigned char>(str[0]))) || (len == 3 && str[0] == '\'' && str[2] == '\''))
       return CHAR;
-    }
     else
     {
-      std::cout << "INT\n";
+      for (size_t i = 0; i < len; i++)
+      {
+        if (i == 0 && (str[i] == '-' || str[i] == '+'))
+          i++;
+        if (!std::isdigit(str[i]))
+          return INVALID;
+      }
       return INT;
     }
   }
   
+  // aqui vai ser o float
   if (f != std::string::npos && dot != std::string::npos)
   {
-    // aqui vai ser o float
-    if (str[f + 1] == '\0')
-      std::cout << "VALIDO\n";
-    std::cout << "Pos do F-> " << f << "\nPos do . -> " << dot << std::endl;
-    return FLOAT;
+    for(size_t i = 0; i < (len - 1); i++)
+    {
+      if (i != dot)
+      {
+        if (!std::isdigit(str[i]))
+          return INVALID;
+      }
+    }
+    if (str[f + 1] == '\0' && (dot + 1) != f)
+      return FLOAT;
+    else
+      return INVALID;
   }
 
+  // aqui vai ser o double
   if (f == std::string::npos && dot != std::string::npos)
   {
-    // aqui vai ser o double
+    for (size_t i = 0; i < len; i++)
+    {
+      if (i != dot)
+      {
+        if (!std::isdigit(str[i]))
+          return INVALID;
+      }
+    }
+    if (str[dot + 1] == '\0')
+      return INVALID;
+    else
+      return DOUBLE;
   }
   return INVALID;
 }
 
+static void printChar(const std::string &str, size_t len)
+{
+  char c;
+  if (len == 3)
+    c = static_cast<unsigned char>(str[1]);
+  else 
+    c = static_cast<unsigned char>(str[0]);
+
+  if (std::isprint(static_cast<int>(c)))
+    std::cout << "char: " << "'" << c << "'" << std::endl;
+  else
+    std::cout << "char: " << "Non displayable" << std::endl;
+  
+  std::cout << "int: " << static_cast<int>(c) << std::endl;
+  std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+  std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
+}
+
+static void printInvalid(void)
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
+}
+
 void ScalarConverter::convert(const std::string &str)
 {
+  if (str.empty())
+  {
+    printInvalid();
+    return;
+  }
   size_t len = str.length();
   int type = getType(str, len);
   switch (type)
   {
     case SPECIAL:
       // printSpecial
+      std::cout << "SPECIAL\n";
       break;
     case CHAR:
       //printChar
+      printChar(str, len);
       break;
     case INT:
       //printInt
+      std::cout << "INT\n";
       break;
     case FLOAT:
       //printFloat
+      std::cout << "FLOAT\n";
       break;
     case DOUBLE:
       //printDouble
+      std::cout << "DOUBLE\n";
       break;
     default:
-      //printInvalid
+      printInvalid();
       break;
   }
-
-  // int len = str.length();
-  // std::cout << len << std::endl;
-  
-  // if(str.empty())
-  // {
-  //     std::cerr << "Error: Empty string provided." << std::endl;
-  //     return;
-  // }
-  // //verificar se e char
-  // if (len == 1 && !std::isdigit(str[0]))
-  // {
-  //   std::cout << "char: " << str << std::endl;
-  //   std::cout << "int: " << str << std::endl;
-  //   std::cout << "float: " << str << std::endl;
-  //   std::cout << "double: " << str << std::endl;
-
-  //   return ;
-  // }
-
 }
-
-/*
-  • char
-  • int
-  • float
-  • double
-*/
