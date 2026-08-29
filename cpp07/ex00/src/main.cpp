da32a45a0254bca89b7d577eb49406f7f91e45c4
Tiago <TiagoDev@TiagoDev.com>
2026-08-29 18:08:33 +0100

    refactor: remove Whatever class; update main logic to include swap functionality and improve output

--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -1,8 +1,20 @@
 
-#include "../inc/Whatever.hpp"
+#include "../inc/whatever.hpp"
 
-int main (int argc, char* argv[])
+int main (void)
 {
+    int a = 10;
+    int b = 5;
+
+    std::cout << "Normal: a-> " << a << " " << "b-> " << b << std::endl;
+
+    std::cout << "min -> " << ::min(a, b) << std::endl;
+    std::cout << "max -> " << ::max(a, b) << std::endl;
+    swap(a, b);
+    std::cout << "With swap: a-> " << a << " " << "b-> " << b << std::endl;
+
+    swap(a, b);
+    std::cout << "Normal again: a-> " << a << " " << "b-> " << b << std::endl;
 
     return 0;
 }
